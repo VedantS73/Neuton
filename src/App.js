66db378c4f50bab676b40f30abc1cf51b6df8190
Aryan Singh <aryansingh6011@gmail.com>
2023-01-15 22:51:07 +0530

    made the layout of landing page

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function App(){
-    return(
-        <div>Chalu kro kam</div>
-    )
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style/landingpg.css';
+import elementbg from './Assets/Elementsvg.svg'
+
+function App(){
+    return(
+        <div className='wrapper'>
+            <div className='wrapper-inner'> 
+            {/* <div className='element'> <img className='moving-image' src={elementbg} /> </div> */}
+            </div>
+        </div>
+        
+    )
+}
+
+export default App;
