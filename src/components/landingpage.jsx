594cbca9d8560c93b757464b36b71e1095e02553
VedantS73 <vedantsawant72003@gmail.com>
2023-01-18 12:04:40 +0530

    background changes

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../style/landingpg.css'
+import Elementbg from '../Assets/Elementsvg.svg'
 
 function background(){
     return(
         
         <div className='wrapper'>
             <div className='wrapper-inner'> 
-            {/* <div className='element'> <img className='moving-image' src={elementbg} /> </div> */}
+            <div className='element'> <img className='moving-image' src={Elementbg} /> </div>
             </div>
         </div>
         
